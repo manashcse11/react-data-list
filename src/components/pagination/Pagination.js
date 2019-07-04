0cff7dc9a0d11157dd59e9a3f0690d6d9e5ae1c8
Manash <manash@cefalo.com>
2019-07-04 12:46:02 +0600

    Show less pagination link

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Pagination = (props) => {
     const {page, total_pages, paginationHandler} = props;
     const pagination_li = [];
-
-    for (let start = 1; start <= total_pages; start++){
+    const pagination_range = 5;
+    const limit = page + pagination_range < total_pages ? page + pagination_range : total_pages;
+    for (let start = page - pagination_range > 0 ? page - pagination_range : 1; start <= limit; start++){
         let li_class = start == page ? "page-item active" : "page-item";
         pagination_li.push(
             <li key={start} className={li_class}><a className="page-link" href="javascript:;" onClick={event => paginationHandler(start)}>{start}</a></li>
